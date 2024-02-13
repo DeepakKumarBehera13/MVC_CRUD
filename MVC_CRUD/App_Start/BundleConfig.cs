ca8f779591eb27401645c2564e79e2b8c7f08170
DESKTOP-GKTTNI9\Deepak <deepakb@centroxy.com>
2024-02-13 17:15:11 +0530

    initial commit

--- MVC_CRUD/App_Start/BundleConfig.cs
+++ MVC_CRUD/App_Start/BundleConfig.cs
@@ -5,7 +5,8 @@ namespace MVC_CRUD
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on , visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on , visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
